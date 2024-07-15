c5e0dbf849e7dbdda19c9ff9a469310c1dda69f3
shittu121 <shittufaruq6@gmail.com>
2024-07-15 22:59:47 +0100

    update card2

--- src/app/components/LandingPage/Cards/CardContent/card2.tsx
+++ src/app/components/LandingPage/Cards/CardContent/card2.tsx
@@ -11,7 +11,7 @@ const Card2: React.FC<CardProps> = ({ title, content, imageurl }) => {
   return (
     <div className="lg:flex md:flex sm:flex block gap-20 w-[100%] relative">
       <Image src={imageurl} className="w-[100%] lg:h-[20rem] md:h-full sm:h-full h-[20rem]" width={50} height={50} alt="cardlogo" />
-      <div className="absolute inset-0 flex flex-col items-center justify-center mt-32">
+      <div className="absolute inset-0 flex flex-col items-center justify-center mt-48">
         <h1 className="text-2xl font-bold text-center">{title}</h1>
         <p className="text-sm lg:text-[9px] text-center py-2 w-48">{content}</p>
       </div>
