f5ba407e782aefdd2c420bae2e1346acd4199ebb
shittu121 <shittufaruq6@gmail.com>
2024-07-16 13:22:36 +0100

    update card2 height

--- src/app/components/LandingPage/Cards/CardContent/card2.tsx
+++ src/app/components/LandingPage/Cards/CardContent/card2.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 const Card2: React.FC<CardProps> = ({ title, content, imageurl }) => {
   return (
     <div className="lg:flex md:flex sm:flex block gap-20 w-[100%] relative">
-      <Image src={imageurl} className="w-[100%] lg:h-full md:h-full sm:h-full h-[35rem]" width={50} height={50} alt="cardlogo" />
+      <Image src={imageurl} className="w-[100%] lg:h-full md:h-[35rem] sm:h-full h-[35rem]" width={50} height={50} alt="cardlogo" />
       <div className="absolute inset-0 flex flex-col items-center justify-center mt-64">
         <h1 className={`text-2xl font-bold text-center  ${title === "Graphic design" ? 'text-[#25564b]' : 'text-[#fad400]'}`}>{title}</h1>
         <p className="text-[18px] lg:text-[13px] text-center py-2 w-48">{content}</p>
